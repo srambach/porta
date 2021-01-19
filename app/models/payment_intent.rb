8d52efda16e1b1248109d78261f9a78ed7521f8d
Guilherme Cassolato <guicassolato@gmail.com>
2021-01-19 18:25:04 +0100

    New API endpoint /api/payment_callbacks/stripe_callbacks
    - Undocumented endpoint used for update callbacks of async-authorized payment transactions (mostly due to SCA regulations)
    - Requires access token with Billing API scope

--- app/models/payment_intent.rb
+++ app/models/payment_intent.rb
@@ -8,4 +8,27 @@ class PaymentIntent < ApplicationRecord
 
   scope :latest, -> (count = 1) { reorder(created_at: :desc).limit(count) }
   scope :latest_pending, ->() { where.not(state: :succeeded).latest }
+
+  scope :by_invoice, ->(invoice_ids) { where(invoice: invoice_ids) }
+
+  def confirm_stripe_payment_intent(event)
+    return unless event.type == 'payment_intent.succeeded'
+
+    payment_intent_data = event.data.object
+
+    transaction do
+      self.state = payment_intent_data['status']
+
+      payment_transaction = invoice.payment_transactions.build({
+        action: :purchase,
+        amount: payment_intent_data['amount'].to_has_money(payment_intent_data['currency']&.upcase || invoice.currency) / 100.0,
+        success: true,
+        message: 'Payment confirmed',
+        reference: payment_intent_data['id'],
+        params: event.to_hash
+      }, without_protection: true)
+
+      save && payment_transaction.save && invoice.pay
+    end
+  end
 end
