c84cf64172c4a3f9c69723de36cf5e46b47b8679
josemigallas <josemigallas@gmail.com>
2021-01-27 18:34:28 +0100

    adds boilerplate for fancy product select

--- app/javascript/src/utilities/patternfly-utils.jsx
+++ app/javascript/src/utilities/patternfly-utils.jsx
@@ -2,14 +2,47 @@
 
 import React from 'react'
 
-import { FormSelectOption } from '@patternfly/react-core'
+import {
+  FormSelectOption,
+  SelectOption,
+  SelectOptionObject as ISelectOptionObject
+} from '@patternfly/react-core'
 
-type Props = {
+interface Record {
   id: string,
-  name: string,
-  disabled?: boolean,
+  name: string
 }
 
-export function toFormSelectOption ({ id, name, disabled = false }: Props) {
-  return <FormSelectOption isDisabled={disabled} key={id} value={id} label={name} />
+type Props = Record & {
+  disabled?: boolean
 }
+
+export const toFormSelectOption = ({ id, name, disabled = false }: Props) => (
+  <FormSelectOption isDisabled={disabled} key={id} value={id} label={name} />
+)
+
+class SelectOptionObject implements ISelectOptionObject {
+  id: string;
+  name: string;
+
+  constructor (item: Record) {
+    this.id = item.id
+    this.name = item.name
+  }
+
+  toString (): string {
+    return this.name
+  }
+
+  compareTo (other: Record): boolean {
+    return this.id === other.id
+  }
+}
+
+export const toSelectOption = ({ id, name, disabled = false }: Props) => (
+  <SelectOption
+    key={id}
+    value={new SelectOptionObject({ id, name })}
+    isDisabled={disabled}
+  />
+)
